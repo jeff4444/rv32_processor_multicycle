02cd7a61bca4297e36b747009dabd77b0b5aab63
Jeff Mofor <allojeff444@gmail.com>
2025-03-01 08:18:06 -0500

    Update first instruction in memory

--- memory.sv
+++ memory.sv
@@ -17,6 +17,20 @@ module memory (
                 mem[i] <= 0;
             end
             // initialize 14 instructions
+            // 03802083 // (Load value 0x5 into x1 from memory 56, lw x1, 56(x0)) 
+            // 00108133 // (x2 = x1 + x1 = 0xA)
+            // 002081B3 // (x3 = x2 + x1 = 0xF)
+            // 40118233 // (x4 = x3 - x1 = 0xA)
+            // 001272B3 // (x5 = x4 & x1 = 0xA & 0x5 = 0x0)
+            // 0011E333 // (x6 = x3 | x1 = 0xF | 0x5 = 0xF)
+            // 00602223 // (Store x6 (0xF) at addr 4)
+            // 00402383 // (Load from addr 4 into x7, should get 0xF)
+            // 40138433 // (x8 = x7 - x1 = 0xF - 0x5 = 0xA)
+            // 0024E4B3 // (x9 = x8 | x2 = 0xA | 0xA = 0xA)
+            // 00902423 // (Store x9 (0xA) at addr 8)
+            // 00802503 // (Load from addr 8 into x10, should get 0xA)
+            // 00950A63 // (Branch if x10 == x1 (0xA == 0x5) - False, no jump)
+            // 00950C63 // (Branch if x10 == x9 (0xA == 0xA) - True, jump +12)
             mem[0] <= 8'h83;
             mem[1] <= 8'h20;
             mem[2] <= 8'h00;
@@ -73,6 +87,8 @@ module memory (
             mem[53] <= 8'h0C;
             mem[54] <= 8'h95;
             mem[55] <= 8'h00;
+
+            mem[56] <= 8'h05;
 
         end else begin
             if (memWrite) begin
